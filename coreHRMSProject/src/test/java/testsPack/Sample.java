f3c84077a0e95ae719d653570b7c103b4d976cdb
ShrinathMateti <shrinathrmateti@gmail.com>
2025-05-23 21:03:51 +0530

    Worked on review comments on local branch shrinath_delete_employee

--- coreHRMSProject/src/test/java/testsPack/Sample.java
+++ coreHRMSProject/src/test/java/testsPack/Sample.java
@@ -3,5 +3,6 @@ package testsPack;
 public class Sample {
     public static void main(String [] args) {
     	System.out.println("Welcome to Github");
+    	System.out.println("Updated the changes");
     }
 }
